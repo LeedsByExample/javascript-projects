00afbcd561962f6c561f0a08eae8e7bc8d6da4e3
LeedsByExample <leeda.minkin@yahoo.com>
2023-09-17 17:47:53 -0500

    Completed Exercises: Arrays. Was super boss about it.

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -2,10 +2,22 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
+console.log(str.split()); //makes the entire string into one entry in an array
+console.log(str.split('')); //make each character in the string, including whitespace, into separate items in an array
+console.log(str.split(' ')); //makes each word in the string into a separate item in an array
+console.log(str.split('e')); //makes separate items in an array, removing 'e' and making the words before/after separate items
+console.log(str);
 
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
+console.log(arr.join()); //combines items into a string with commas and no whitespace
+console.log(arr.join('')); //combines items into string with no commas and no whitespace
+console.log(arr.join(' ')); //combines items into a string with whitespace but no commas
+console.log(arr.join('a')); //combines items into a string with no whitespace and add's 'a' in between each item
+console.log(arr);
 
 //3) Do split or join change the original string/array?
+//split and join do not change the original string/array
 
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+console.log(cargoHold.split(',').sort().join(', '));
--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -4,7 +4,15 @@ let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
 //1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
-
+console.log(holdCabinet1.concat(holdCabinet2));
+console.log(holdCabinet2);
 //2) Print a slice of two elements from each array. Does slice alter the original arrays?
+console.log(holdCabinet1.slice(0,1));
+console.log(holdCabinet1);
+//concat() and slice() do not alter the original arrays
 
 //3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+console.log(holdCabinet1.reverse()); //reverse simply reverses the order of the items
+console.log(holdCabinet2.sort()); //sort sorts the items based on specific criteria (starting with first number or letter of an item?)
+
+//Array.splice(start, removeCount, newItem, newItem, newItem, ...)
