491995ac7ec464e54b56a2517567f9f5a9122780
LeedsByExample <leeda.minkin@yahoo.com>
2023-09-18 21:49:46 -0500

    Completed String Modification and Multi Dimensional Arrays Studios. Was super dope about it.

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -1,14 +1,51 @@
+const input = require('readline-sync');
+
 let food = "water bottles,meal packs,snacks,chocolate";
 let equipment = "space suits,jet packs,tool belts,thermal detonators";
 let pets = "parrots,cats,moose,alien eggs";
 let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
+cabinet0 = food.split(",").sort();
+cabinet1 = equipment.split(",").sort();
+cabinet2 = pets.split(",").sort();
+cabinet3 = sleepAids.split(",").sort();
 
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
+let cargoHold = [cabinet0,cabinet1,cabinet2,cabinet3];
+console.log(cargoHold);
 
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
+userCabinet = Number(input.question("Please select a cabinet from 0 to 3: "));
+
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
+if (userCabinet == 0 || userCabinet == 1 || userCabinet == 2 || userCabinet == 3) {
+    console.log(`You have selected: Cabinet ${userCabinet}.`);
+    if (userCabinet == 0) {
+        console.log(cargoHold[0]);
+    }
+    if (userCabinet == 1) {
+        console.log(cargoHold[1]);
+    }
+    if (userCabinet == 2) {
+        console.log(cargoHold[2]);
+    }
+    if (userCabinet == 3) {
+        console.log(cargoHold[3]);
+    }
+} else {
+    console.log("Error. Please enter a valid number 0-3.");
+    userCabinet = input.question("Please select a cabinet from 0 to 3: ");
+}
 
-//5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+userItem = input.question("Now, please select an item: ");
+if (cargoHold[userCabinet].includes(userItem)) {
+    console.log(`Cabinet ${userCabinet} DOES contain ${userItem}.`);
+} else {
+    console.log(`Cabinet ${userCabinet} DOES NOT contain ${userItem}.`);
+}
+
+
+//5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. 
+//Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,9 +3,31 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
+let str2 = str.slice(0,3);
+let str3 = str.slice(3,str.length);
+let finalString = (str3.concat('', str2));
+console.log(finalString);
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
-
+console.log(`The original string was "${str}". The modified string is "${finalString}".`)
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+userInput = Number(input.question("How many letters would you like moved from the start of the word LaunchCode to the end? "));
+if (userInput > str.length) {
+    str2 = str.slice(0,3);
+    str3 = str.slice(3,str.length);
+    finalString = (str3 + str2);
+    console.log(`You selected an invalid number. We have defaulted to moving 3 characters. The original string was ${str}. The modified string is ${finalString}`);
+} else if (userInput < 0) {
+    console.log("Invalid input. Select again.");
+} else {
+    str2 = str.slice(0,userInput);
+    str3 = str.slice(userInput,str.length);
+    finalString = (str3 + str2);
+    console.log(`The original string was ${str}. The modified string is ${finalString}.`);
+}
+//3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. 
+//Also, the template literal should note the error.
 
-//3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+
+//Array.splice(start, removeCount, newItem, newItem, newItem, ...)
+
