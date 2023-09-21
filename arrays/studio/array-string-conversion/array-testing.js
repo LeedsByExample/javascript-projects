f5e0f2647b59476fa3131ed53836b6887f0e7a93
LeedsByExample <leeda.minkin@yahoo.com>
2023-09-20 21:52:44 -0500

    Completed Array Testing Studio. Was super neat about it.

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -8,42 +8,55 @@ strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 //2) 
 function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
-	let check;
-	let output;
+	
 	//TODO: 2. write the code required for this step
-
+	protoArray1.includes(",");
+	console.log(protoArray1.split(",").reverse().join());
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
-	return output;
+	
 }
+reverseCommas();
 
 //3)
 function semiDash() {
 	let check;
 	let output;
 //TODO: write the code required for this step
-
+	if (protoArray2.includes(";")) {
+		console.log(protoArray2.split(";").sort().join("-"));
+	} else {
+		return;
+	}
   
 	return output;
 }
-
+semiDash();
 //4)
 function reverseSpaces() {
 	let check;
 	let output;
   //TODO: write the code required for this step
-
+	if (protoArray3.includes(' ')) {
+		console.log(protoArray3.split(' ').sort().join(' '));
+	} else {
+		return;
+	}
 	return output;
 }
-
+reverseSpaces();
 //5)
 function commaSpace() {
 	let check;
 	let output;
 	//TODO: write the code required for this step
-  
+  if (protoArray4.includes(", ")) {
+	console.log(protoArray4.split(", ").sort().join(", "));
+  } else {
+	return;
+  }
 	return output;
 }
-
+commaSpace();
 // NOTE: Don't add or modify any code below this line or your program might not run as expected.
 module.exports = {
 	strings : strings, 
