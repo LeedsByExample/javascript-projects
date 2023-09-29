c1e0519a038752b00e25ed9bdd1a32a63095cdcc
LeedsByExample <leeda.minkin@yahoo.com>
2023-09-28 19:35:16 -0500

    Completed part-one-find-minimum-value.

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -1,4 +1,18 @@
-//1) Create a function with an array of numbers as its parameter. The function should iterate through the array and return the minimum value from the array. Hint: Use what you know about if statements to identify and store the smallest value within the array.
+//1) Create a function with an array of numbers as its parameter. The function should iterate through the array and return the minimum 
+//value from the array. Hint: Use what you know about if statements to identify and store the smallest value within the array.
+
+function findMinVal(nums) {
+    let minVal = nums[0];
+    for (let i = 0; i < nums.length; i++) {
+        
+        if (nums[i] < minVal) {
+            minVal = nums[i];
+        }
+
+    } return minVal;
+}
+
+
 
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
@@ -7,4 +21,17 @@ let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Using one of the test arrays as the argument, call your function inside the console.log statement below.
 
-console.log(/* your code here */);
+console.log(findMinVal(nums1));
+console.log(findMinVal(nums2));
+console.log(findMinVal(nums3));
+
+// minVal = Infinity; //2
+        // maxVal = 0; //10
+        
+        // if (nums[i] < minVal) {
+        //     minVal = nums[i];
+        //     // return minVal;
+        // } else if (nums[i] > minVal) {
+        //     maxVal = nums[i];
+        //     // return maxVal;
+        // } return minVal;
