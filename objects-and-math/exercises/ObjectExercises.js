06314cfee007aebd860046095dc7c2ab0a0a2ac7
LeedsByExample <leeda.minkin@yahoo.com>
2023-10-01 20:18:52 -0500

    Worked on object exercises, code still not running properly even when textbook solution used

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -2,21 +2,70 @@ let superChimpOne = {
    name: "Chad",
    species: "Chimpanzee",
    mass: 9,
-   age: 6
+   age: 6,
+   astronautID: 1,
+   move: function () {return Math.floor(Math.random()*11)}
 };
+console.log(superChimpOne);
 
 let salamander = {
    name: "Lacey",
    species: "Axolotl Salamander",
    mass: 0.1,
-   age: 5
+   age: 5,
+   astronautID: 2,
+   move: function () {return Math.round(Math.random()*10)}
 };
 
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6,
+   astronautID: 3,
+   move: function () {return Math.round(Math.random()*10)}
+};
 
-// After you have created the other object literals, add the astronautID property to each one.
+let dog = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5,
+   astronautID: 4,
+   move: function () {return Math.round(Math.random()*10)}
+};
 
-// Create an array to hold the animal objects.
+let waterBear = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1,
+   astronautID: 5,
+   move: function () {return Math.round(Math.random()*10)}
+};
 
-// Print out the relevant information about each animal.
+let crew = [superChimpOne, salamander, superChimpTwo, dog, waterBear];
 
-// Start an animal race!
+function crewReports(animal) {
+   console.log(`${animal.name} is a ${animal.species}. They are ${animal.age} years old and ${animal.mass} kilograms. Their ID is ${animal.astronautID}.`);
+}
+
+crewReports(dog);
+
+//Make a crew fitness race function
+
+function fitnessTest(candidates){
+   let results = [], numSteps, turns;
+   for (let i = 0; i < candidates.length; i++){
+       numSteps = 0;
+       turns = 0;
+         while(numSteps < 20){
+         numSteps += candidates[i].move();
+         turns++;
+       }
+       results.push(`${candidates[i].name} took ${turns} turns to take 20 steps.`);
+   }
+   return results;
+  }
+
+fitnessTest(crew);
