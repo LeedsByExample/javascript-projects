ec584ebb1ab9f1d9cbfc4ca2c0dc884fe3472ae9
LeedsByExample <leeda.minkin@yahoo.com>
2023-10-12 20:18:18 -0500

    Completed Loops Studio. Was super late about it.

--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -2,12 +2,11 @@ const input = require('readline-sync');
 
 // Part A: #1 Populate these arrays
 
-let protein = [];
-let grains = [];
-let veggies = [];
-let beverages = [];
-let desserts = [];
-
+let protein = ["chicken", "pork", "tofu", "beef", "fish", "beans"];
+let grains = ["rice", "pasta", "corn", "potato", "quinoa", "crackers"];
+let veggies = ["peas", "green beans", "kale", "edamame", "broccoli", "asparagus"];
+let beverages = ["juice", "milk", "water", "soy milk", "soda", "tea"];
+let desserts = ["apple", "banana", "more kale", "ice cream", "chocolate", "kiwi"];
 
 function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   let pantry = [protein, grains, veggies, beverages, desserts];
@@ -15,14 +14,43 @@ function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   
   /// Part A #2: Write a ``for`` loop inside this function
   /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
+  
+  // for (let i = 0; i < numMeals.length; i++) {
+  //   let meal = [];
+  //   meal.push(pantry[i]);
 
+  //   for (let j = 0; j < 6; j++) {
+  //     //insert code here
+  //   }
+  // };
 
+  for(let i = 0; i < numMeals; i++) {
+      meals.push([]);
+          
+      for(let j = 0; j < 5; j++) {
+          if(j === 0) {
+            meals[i].push(protein[i]);
+          } else if(j === 1) {
+            meals[i].push(grains[i]);
+          } else if(j === 2) {
+            meals[i].push(veggies[i]);
+          } else if(j === 3) {
+            meals[i].push(beverages[i]);
+          } else {
+            meals[i].push(desserts[i]);
+            }
+          }
+          
+        }
+
+  // for (let i = 0; i < 5; i++) {
+  //   console.log(protein[i]);
+  // }
   return meals;
 }
 
-
 function askForNumber() {
-  numMeals = input.question("How many meals would you like to make?");
+  numMeals = input.question("How many meals would you like to make? ");
   
   /// CODE YOUR SOLUTION TO PART B here ///
 
@@ -32,6 +60,10 @@ function askForNumber() {
 
 function generatePassword(string1, string2) {
   let code = '';
+  for (let i = 0; i < string1.length; i++) {
+    code += string1.slice(i, i+1);
+    code += string2.slice(i, i+1);
+  }
 
   /// Code your Bonus Mission Solution here ///
 
@@ -45,25 +77,26 @@ function runProgram() {
   /// Change the final input variable (aka numMeals) here to ensure your solution makes the right number of meals ///
   /// We've started with the number 2 for now. Does your solution still work if you change this value? ///
   
-  // let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
-  // console.log(meals)
+  let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
+  console.log(meals);
   
 
   /// TEST PART B HERE ///
   /// UNCOMMENT the next two lines to test your ``askForNumber`` solution ///
   /// Tip - don't test this part until you're happy with your solution to part A #2 ///
   
-  // let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
-  // console.log(mealsForX);
+  let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
+  console.log(mealsForX);
 
     /// TEST PART C HERE ///
   /// UNCOMMENT the remaining commented lines and change the password1 and password2 strings to ensure your code is doing its job ///
 
-  // let password1 = '';
-  // let password2 = '';
-  // console.log("Time to run the password generator so we can update the menu tomorrow.")
-  // console.log(`The new password is: ${generatePassword(password1, password2)}`);
+  let password1 = 'hello';
+  let password2 = 'there';
+  console.log("Time to run the password generator so we can update the menu tomorrow.")
+  console.log(`The new password is: ${generatePassword(password1, password2)}`);
 }
+runProgram();
 
 module.exports = {
   protein: protein,
