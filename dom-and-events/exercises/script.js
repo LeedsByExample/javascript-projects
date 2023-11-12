132e14fc7010ea515dc22b75acb655709e533775
LeedsByExample <leeda.minkin@yahoo.com>
2023-11-11 20:37:38 -0600

    Completed DOM and Events exercises.

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,7 +3,28 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
+    function takeOff() {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+        console.log("Houston, we have liftoff!");
+    };
+    button.addEventListener("click", takeOff);
+
+    missionAbort.addEventListener("mouseover", function(event) {
+        event.target.className = 'redAbortButton';
+    });
+
+    missionAbort.addEventListener("mouseout", function(event) {
+        event.target.className = "";
+    })
+
+    missionAbort.addEventListener("click", function(event) {
+        let confirmAbort = window.confirm("Are you sure you want to abort the mission?");
+        if (confirmAbort) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home.";
+        } else {
+            paragraph.innerHTML = "k then why'd you click the button";
+        }
+    })
     
 }
 
