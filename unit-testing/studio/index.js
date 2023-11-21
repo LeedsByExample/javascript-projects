4e3e26a5d656457fff5faaff1d309bf8e460c3a5
LeedsByExample <leeda.minkin@yahoo.com>
2023-11-20 18:28:19 -0600

    Completed unit testing studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -4,10 +4,35 @@ let launchcode = {
   executiveDirector: "Jeff",
   percentageCoolEmployees: 100,
   programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
-  launchOutput: function() {
-    
+  launchOutput: function(num) {
+    let str = "";
+
+    if (num % 2 === 0) {
+      str += "Launch"
+    } 
+
+    if (num % 3 === 0) {
+      str += "Code";
+    } 
+
+    if (num % 5 === 0) {
+      if (str.length > 0) {
+        str += " ";
+      }
+      str += "Rocks";
+    } 
+
+    if (str.length === 0) {
+      return "Rutabagas! That doesn't work."
+    } else if (str === "Launch Rocks") {
+      return str += " (CRASH!!!!)"
+    } else {
+      return str;
+    }
   }
 }
+
+console.log(launchcode.launchOutput(10));
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -25,12 +25,47 @@ describe("testing launchcode", function() {
     expect(launchcode.programsOffered.length).toBe(3);
   });
 
-  //When passed a number that is ONLY divisible by 2, launchOutput() returns 'Launch!'
-  test("should return 'Launch!' when passed a number ONLY divisible by 2", function() {
-    expect(launchOutput(2)).toBe('Launch!');
+  //When passed a number that is ONLY divisible by 2, launchOutput() returns 'Launch'
+  test("should return 'Launch' when passed a number ONLY divisible by 2", function() {
+    expect(launchcode.launchOutput(2)).toBe('Launch');
   });
-});
 
+  // When passed a number that is ONLY divisible by 3, return 'Code'
+  test("should return 'Code' when passed a number ONLY divisible by 3", function() {
+    expect(launchcode.launchOutput(3)).toBe('Code');
+  });
+  // When passed a number that is ONLY divisible by 5, return 'Rocks'
+  test("should return 'Rocks' when passed a number ONLY divisible by 5", function() {
+    expect(launchcode.launchOutput(5)).toBe('Rocks');
+  })
+  
+  // When passed a number that is divisible by 2 AND 3, return 'LaunchCode
+  test("should return 'LaunchCode' when passed a number divisible by 2 AND 3", function() {
+    expect(launchcode.launchOutput(6)).toBe('LaunchCode');
+  })
+
+  // When passed a number that is divisible by 3 AND 5, return 'Code Rocks!'
+  test("should return 'Code Rocks' when passed a number divisible by 3 AND 5", function() {
+    expect(launchcode.launchOutput(15)).toBe('Code Rocks');
+  })
+
+  // When passed a number that is divisible by 2 AND 5, return 'Launch Rocks!'
+  test("should return 'Launch Rocks' when passed a number divisible by 2 AND 5", function() {
+    expect(launchcode.launchOutput(10)).toBe('Launch Rocks (CRASH!!!!)');
+  })
+  
+  // When passed a number that is divisible by 2, 3, AND 5, return 'LaunchCode Rocks!'
+  test("should return 'LaunchCode Rocks' when passed a number divisible by 2, 3 AND 5", function() {
+    expect(launchcode.launchOutput(30)).toBe('LaunchCode Rocks');
+  })
+
+
+  // When passed a number that is NOT divisible by 2, 3, or 5, return 'Rutabagas! That doesn't work.'
+  test("should return 'Rutabagas! That doesn't work.' when passed a number NOT divisible by 2, 3, or 5", function() {
+    expect(launchcode.launchOutput(67)).toBe("Rutabagas! That doesn't work.");
+  })
+
+});
 
 
 
